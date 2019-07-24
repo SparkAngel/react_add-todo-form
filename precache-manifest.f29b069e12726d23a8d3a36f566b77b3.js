7a621da40e40510089ed9de6086a44297b6c529a
SparkAngel <igor.gnatenko555@gmail.com>
2019-07-24 15:54:20 +0300

    Updates

---
+++ precache-manifest.f29b069e12726d23a8d3a36f566b77b3.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "afce3377ce9a2dfba7edb24d695a94d7",
+    "url": "/react_add-todo-form/index.html"
+  },
+  {
+    "revision": "71b44bc2ad30b9636e6f",
+    "url": "/react_add-todo-form/static/css/main.74601814.chunk.css"
+  },
+  {
+    "revision": "aef2a7fe1c89243aee7e",
+    "url": "/react_add-todo-form/static/js/2.053d81dc.chunk.js"
+  },
+  {
+    "revision": "71b44bc2ad30b9636e6f",
+    "url": "/react_add-todo-form/static/js/main.a044065e.chunk.js"
+  },
+  {
+    "revision": "4953b3d381bec0cda918",
+    "url": "/react_add-todo-form/static/js/runtime~main.9d188fa2.js"
+  }
+]);
